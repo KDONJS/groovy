bcb7ad52b5a9b51bc5b2d34cddf30586f3695389
KDONJS <u19213103@utp.edu.pe>
2022-08-15 11:13:39 -0500

    sin steps

--- file-jenkins.groovy
+++ file-jenkins.groovy
@@ -1,43 +1,30 @@
-def metodo(parametro) {
-    return parametro
-}
-
 def project = "KDON"
 //def buildNumber = BUILD_NUMBER
 //def projectPeriod = "kdon." + buildNumber
 
 node(){
     stage('Instalacion'){
-        steps(){
             //sh 'yum -y install cowsay'
             //sh 'yum -y install lolcat'
-
-            sh 'echo "Instalando dependencias..."'
-        }
+        sh 'echo "Instalando dependencias..."'
     }
     stage('Ejecución'){
-        steps(){
-            sh 'cowsay "KDON"|lolcat'
-        }
+        sh 'cowsay "KDON"|lolcat'
     }
     stage('Variables'){
         if(project == "KDON"){
             stage('Variable si'){
-                steps(){
-                    sh 'cowsay "KDON"|lolcat'
-                }
+                sh 'cowsay "KDON"|lolcat'
             }
         }else{
             stage('Variable no'){
-                steps(){
-                    sh 'cowsay "KDON"|lolcat'
-                }
+                sh 'cowsay "KDON"|lolcat'
             }
         }
     }
     stage('loop de entorno'){
         for(i = 0 ; i < 10 ; i++){
-                echo "Entorno: " + i
+            echo "Entorno: " + i
         }
     }
 }
