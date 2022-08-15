d531264d76764ec16813b0bc8012d7bedded103e
KDONJS <u19213103@utp.edu.pe>
2022-08-15 10:19:04 -0500

    mejora de mensaje

--- file-jenkins.groovy
+++ file-jenkins.groovy
@@ -2,39 +2,40 @@ def metodo(texto) {
    echo texto
 }  
 
-node {
-    texto = 'Hola' //Variables string con comillas simples o dobles
-    numero = 990  //Puedo o no se indica el tipo de variable
-    numero ++
-    echo texto + " " + numero
-    
-    
-    if(numero <= 900){
-        stage('prueba dif'){
-        echo "hola esta es una prueba de if"
-        }
-    }
-    }else{
-        stage('prueba no if'){
-        echo "hola esta es una prueba de no if"
-    }
-    }
-    
-    stage('Ejemplo') {
-        valor = 1000
-        if (numero >= valor) {
-            echo 'en if'
-        }else{
-            echo 'esto no es verdad'
-        } 
+try {
+   node(){
+        texto = 'Hola' //Variables string con comillas simples o dobles
+        numero = 990  //Puedo o no se indica el tipo de variable
+        numero ++
+        echo texto + " " + numero
         
-        for (i = 0; i< 10; i++) {
-            echo "For: " + i
+        
+        if(numero <= 900){
+            stage('prueba dif'){
+            echo "hola esta es una prueba de if"
         }
-    }
-    stage("Saludando") {
-        metodo("como estas yorlin")
-    }
+        }else{
+            stage('prueba no if'){
+            echo "hola esta es una prueba de no if"
+        }
+        }
+        
+        stage('Ejemplo') {
+            valor = 1000
+            if (numero >= valor) {
+                echo 'en if'
+            }else{
+                echo 'esto no es verdad'
+            } 
+            
+            for (i = 0; i< 10; i++) {
+                echo "For: " + i
+            }
+        }
+        stage("Saludando") {
+            metodo("como estas yorlin")
+        }
     
 
+    }
 }
